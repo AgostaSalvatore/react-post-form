2ad8ec24a1c558681422cd05c887dda5e656ceb3
AgostaSalvatore <agosta.boolean@gmail.com>
2025-05-14 14:15:52 +0200

    feat: initialize post form app with basic structure and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Hook
+import { useState, useEffect } from "react"
+// Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"
+//axios
+import axios from "axios"
+
+//endpoint
+const endpoint = 'https://67c5b4f3351c081993fb1ab6.mockapi.io/api/posts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const initialFormPost = {
+    author: '',
+    title: '',
+    body: '',
+    public: false
+  }
+
+  const [formPosts, setFormPosts] = useState(initialFormPost)
+
+  function handleFormPosts(event) {
+    const checkboxValue = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+
+    setFormPosts(formPost => ({
+      ...formPost,
+      [event.target.post]: event.target.value
+    }))
+
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <h1>Post Form</h1>
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
